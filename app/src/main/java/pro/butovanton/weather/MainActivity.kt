d0eb64ede69c78af039eeff47fa68e63f91efc73
Anton Butov <butov6101@gmail.com>
2020-08-03 20:41:11 +0300

    Main spinner notify onResume

--- app/src/main/java/pro/butovanton/weather/MainActivity.kt
+++ app/src/main/java/pro/butovanton/weather/MainActivity.kt
@@ -3,21 +3,42 @@ package pro.butovanton.weather
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var buttonSytes : Button
+    lateinit var buttonCytes : Button
+    lateinit var model: MainViewModel
+    lateinit var spinner : Spinner
+    lateinit var spinnerArray: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonSytes = findViewById(R.id.buttonSytes)
-        buttonSytes.setOnClickListener() {
+        buttonCytes = findViewById(R.id.buttonSytes)
+        buttonCytes.setOnClickListener() {
             view -> val intent = Intent(this, ActivitySitys:: class.java)
                         startActivity(intent)
         }
+
+        model = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        spinnerArray = model.getCitysNames()
+        val spinnerArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerArray)
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner = findViewById(R.id.spinnerCity)
+        spinner.adapter = spinnerArrayAdapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        spinnerArray = model.getCitysNames()
     }
 }
--- app/src/main/java/pro/butovanton/weather/MainViewModel.kt
+++ app/src/main/java/pro/butovanton/weather/MainViewModel.kt
@@ -4,4 +4,16 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
+    lateinit var citys : List<City>
+    var city : Int = 0
+    val repo = Repo.instance
+    val cityNames: MutableList<String> = mutableListOf()
+
+    fun getCitysNames() : List<String> {
+        citys = repo.getCitys()
+        cityNames.clear()
+        for (city in citys)
+            cityNames.add(city.name)
+    return cityNames
+    }
 }
